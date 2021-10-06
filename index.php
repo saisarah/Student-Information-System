ecfc245c388aab2314520c91664be3e988272cf2
Sai Sarah <sarahoben7@gmail.com>
2021-10-06 16:07:40 +0800

    Admin login

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Admin</title>
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="assets/bootstrap-4/css/bootstrap.min.css">
+
+    <script src="https://kit.fontawesome.com/b1b0cba1bb.js" crossorigin="anonymous"></script> 
+  </head>
+  <body>
+    
+    <div class="container">
+      <div class="wrapper consas" id="consas1" style="width: 400px; margin-left: 35%;">
+        <div class="title"><span>Admin Login</span>
+        </div>
+        <form class="form">
+          <div class="row">
+            <i class="fa fa-user"></i>
+            <input type="text" placeholder="Username" id="username">
+          </div>
+          <div class="row">
+            <i class="fa fa-lock"></i>
+            <input type="password" placeholder="Password" id="password">
+          </div>
+          <div class="row button">
+            <input type="submit" id="btnlogin"></a>
+          <div class="alert alert-custom text-center" style="margin-top:5px;" role="alert"></div>
+          </div>
+        </form>
+      </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
+
+  </body>
+</html>
