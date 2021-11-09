6f526bd4aa71af58c82c7731292cf88d44b9962f
Sai Sarah <sarahoben7@gmail.com>
2021-11-09 23:01:21 +0800

    Passing of data

--- assets/js/ajax-script.js
+++ assets/js/ajax-script.js
@@ -1,4 +1,5 @@
 
+
       $.ajax({    
         type: "GET",
         url: "modules/stud-list.php",
@@ -8,3 +9,63 @@
            
         }
     });
+(function() {
+
+    var app = {
+        initialize: function () {
+            this.createAcc.initialize();
+        },
+         
+        createAcc: {
+            initialize: function() {
+                this.registerNewUser();
+            },
+            registerNewUser: function() {
+                var btnAddStud = jQuery('#btnAddStud');
+
+                btnAddStud.click(function() {
+                    var firstName = jQuery('#inputfName').val().toUpperCase(),
+                        lastName = jQuery('#inputlName').val().toUpperCase(),
+                        midName = jQuery('#inputmName').val().toUpperCase(),
+                        course = jQuery('#inputCourse').val().toUpperCase(),
+                        year = jQuery('#inputYear').val().toUpperCase(),
+                        section = jQuery('#inputSection').val().toUpperCase(),
+                        studNo = jQuery('#inputStudno').val(),
+                        validation_message = jQuery('.alert');
+
+                        
+
+                        // Send details to DB
+                        jQuery.ajax({
+                            url: "modules/add-stud.php",
+                            type: "post",
+                            data: {
+                                firstName : firstName,
+                                lastName : lastName,
+                                midName : midName,
+                                course : course,
+                                year : year,
+                                section : section,
+                                studNo : studNo
+                            },
+                            success: function(data) {
+                                if (data) {
+                                    validation_message.hide();
+                                    alert(data);
+                                    window.location.href = 'student.php?registration=' + data;
+                                }
+                            }
+                        });
+                    
+                });
+            }
+        },
+        
+       
+    }
+
+    jQuery(document).ready( function() {
+        app.initialize();
+    });
+
+})();
