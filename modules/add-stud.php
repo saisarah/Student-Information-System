7a9b94b8aeac97fdb30a89e153f98161506dfbb7
Sai Sarah <sarahoben7@gmail.com>
2021-12-08 19:24:15 +0800

    Inserting of date

--- modules/add-stud.php
+++ modules/add-stud.php
@@ -12,12 +12,12 @@
     $studNo = $_POST['studNo'];
     $studStat = 'Regular';
     $status = 'Inactive';
-
+    $date = date('Y-m-d H:i:s');
     // Connect to DB ($conn)
     include('dbConn.php');
    
 
-         $sql1 = "INSERT INTO tbl_student (firstname, lastname, midname, course, year, section, studno, status, stud_Stat) VALUES ('".$firstName."', '".$lastName."', '".$midName."', '".$course."', '".$year."', '".$section."', '".$studNo."', '".$status."', '".$studStat."')";
+         $sql1 = "INSERT INTO tbl_student (firstname, lastname, midname, course, year, section, studno, status, stud_Stat, dateInserted) VALUES ('".$firstName."', '".$lastName."', '".$midName."', '".$course."', '".$year."', '".$section."', '".$studNo."', '".$status."', '".$studStat."', '".$date."')";
  
     if ($conn->query($sql1) === TRUE) {
         echo "SUCCESS";
