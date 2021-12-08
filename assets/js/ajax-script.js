c60edd1e0c9e015a041bdd065fb7509dda24240a
Sai Sarah <sarahoben7@gmail.com>
2021-12-08 19:18:48 +0800

    GET method for Dashboard display

--- assets/js/ajax-script.js
+++ assets/js/ajax-script.js
@@ -9,6 +9,24 @@
     });
       $.ajax({    
         type: "GET",
+        url: "modules/dashboard.php",
+        dataType: "html",                  
+        success: function(data){                    
+            $("#table-stud").html(data); 
+           
+        }
+    });
+      $.ajax({    
+        type: "GET",
+        url: "modules/dashboard1.php",
+        dataType: "html",                  
+        success: function(data){                    
+            $("#table-prof").html(data); 
+           
+        }
+    });
+      $.ajax({    
+        type: "GET",
         url: "modules/acc-Created.php",
         dataType: "html",                  
         success: function(data){                    
