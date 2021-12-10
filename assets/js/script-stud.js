fd2c1986a90e0cd55dec85372835cdfbf48d5f1f
Sai Sarah <sarahoben7@gmail.com>
2021-12-11 00:57:14 +0800

    Validation

--- assets/js/script-stud.js
+++ assets/js/script-stud.js
@@ -21,41 +21,95 @@
             registerNewUser: function() {
                 var btnAddStud = jQuery('#btnAddStud');
 
-                btnAddStud.click(function() {
-                    var firstName = jQuery('#inputfName').val().toUpperCase(),
-                        lastName = jQuery('#inputlName').val().toUpperCase(),
+             //   btnAddStud.click(function() {
+              //      var firstName = jQuery('#inputfName').val().toUpperCase(),
+                  //      lastName = jQuery('#inputlName').val().toUpperCase(),
+                    //    midName = jQuery('#inputmName').val().toUpperCase(),
+                 //       course = jQuery('#inputCourse').val().toUpperCase(),
+                    //    year = jQuery('#inputYear').val().toUpperCase(),
+                 //       section = jQuery('#inputSection').val().toUpperCase(),
+                    //    studNo = jQuery('#inputStudno').val(),
+                    //    validation_message = jQuery('.alert');
+
+                          $("#modal").validate({
+                            rules: {
+                              inputfName: {
+                                required: true,
+                              },
+                              inputlName: {
+                                required: true,
+                              },
+                              inputCourse: {
+                                required: true,
+                              },
+                              inputYear: {
+                                required: true,
+                              },
+                              inputSection: {
+                                required: true,
+                              },
+                              inputStudno: {
+                                required: true,
+                                minlength: 8
+                              },
+                              action: "required"
+                            },
+                            messages: {
+                              inputfName: {
+                                required: "Please enter some data",
+                              },
+                              inputlName: {
+                                required: "Please enter some data",
+                              },
+                              inputCourse: {
+                                required: "Please enter some data",
+                              },
+                              inputYear: {
+                                required: "Please enter some data",
+                              },
+                              inputSection: {
+                                required: "Please enter some data",
+                              },
+                              inputStudno: {
+                                required: "Please enter some data",
+                                minlength: "Student number must be 8 Characters"
+                              },
+                              action: "Please provide some data"
+                            },
+                            submitHandler: function (form) {
+                                 var firstName = jQuery('#inputfName').val().toUpperCase(),
+                      lastName = jQuery('#inputlName').val().toUpperCase(),
                         midName = jQuery('#inputmName').val().toUpperCase(),
-                        course = jQuery('#inputCourse').val().toUpperCase(),
+                       course = jQuery('#inputCourse').val().toUpperCase(),
                         year = jQuery('#inputYear').val().toUpperCase(),
                         section = jQuery('#inputSection').val().toUpperCase(),
                         studNo = jQuery('#inputStudno').val(),
                         validation_message = jQuery('.alert');
+                            // Send details to DB
 
-                        
-
-                        // Send details to DB
-                        jQuery.ajax({
-                            url: "modules/add-stud.php",
-                            type: "post",
-                            data: {
-                                firstName : firstName,
-                                lastName : lastName,
-                                midName : midName,
-                                course : course,
-                                year : year,
-                                section : section,
-                                studNo : studNo
-                            },
-                            success: function(data) {
-                                if (data) {
+                            jQuery.ajax({
+                                url: "modules/add-stud.php",
+                                type: "post",
+                                data: {
+                                    firstName : firstName,
+                                    lastName : lastName,
+                                    midName : midName,
+                                    course : course,
+                                    year : year,
+                                    section : section,
+                                    studNo : studNo
+                                },
+                                success: function(data) {
+                                    if (data) {
                                     validation_message.hide();
                                     alert(data);
-                                    window.location.href = 'student.php?registration=' + data;
+                                    window.location.href = 'student.php?registration=successfully';
                                 }
                             }
-                        });
-                    
-                });
+                        }); //END jQuery
+                    } // END SUBMIT HANDLER
+                }); // END VALIDATE
+                    //  });
             }
         },
         
--- modules/add-stud.php
+++ modules/add-stud.php
@@ -15,17 +15,26 @@
     $date = date('Y-m-d H:i:s');
     // Connect to DB ($conn)
     include('dbConn.php');
-   
 
-         $sql1 = "INSERT INTO tbl_student (firstname, lastname, midname, course, year, section, studno, status, stud_Stat, dateInserted) VALUES ('".$firstName."', '".$lastName."', '".$midName."', '".$course."', '".$year."', '".$section."', '".$studNo."', '".$status."', '".$studStat."', '".$date."')";
- 
-    if ($conn->query($sql1) === TRUE) {
-        echo "SUCCESS";
-    } else {
-        echo "FAILED";
+    $sql = "SELECT * FROM tbl_student WHERE studno = '". $studNo ."'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0){
+        //Retrieving data from data table
+        while($row = $result->fetch_assoc()) {
+            $stud = $row['studno'];
+        }
+        if ($studNo == $stud) {
+            echo "Student number already existed";
+        }
+    }else {
+        $sql1 = "INSERT INTO tbl_student (firstname, lastname, midname, course, year, section, studno, status, stud_Stat, dateInserted) VALUES ('".$firstName."', '".$lastName."', '".$midName."', '".$course."', '".$year."', '".$section."', '".$studNo."', '".$status."', '".$studStat."', '".$date."')";
+            if ($conn->query($sql1) === TRUE) {
+                echo "SUCCESS";
+            } else {
+                echo "FAILED";
+           
+            }
     }
-    
-   
     // Close connection ($conn)
     $conn->close();
 
