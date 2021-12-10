991a1ab007a43b109b4b377bc6f27bcf332c9882
Sai Sarah <sarahoben7@gmail.com>
2021-12-11 01:01:58 +0800

    Added Table for Schedule and Subjects

--- database/dbsis.sql
+++ database/dbsis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2021 at 12:30 PM
+-- Generation Time: Dec 10, 2021 at 06:00 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
@@ -57,12 +57,31 @@ CREATE TABLE `tbl_professor` (
   `dateInserted` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `tbl_professor`
+-- Table structure for table `tbl_schedule`
 --
 
-INSERT INTO `tbl_professor` (`id`, `firstname`, `lastname`, `department`, `username`, `password`, `status`, `dateInserted`) VALUES
-(1, 'JEFFREY', 'COCO', 'CSD', '', '', '', '0000-00-00');
+CREATE TABLE `tbl_schedule` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `instructor` varchar(255) NOT NULL,
+  `day` varchar(255) NOT NULL,
+  `startT` varchar(255) NOT NULL,
+  `endT` varchar(255) NOT NULL,
+  `course` varchar(255) NOT NULL,
+  `year` int(255) NOT NULL,
+  `section` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_schedule`
+--
+
+INSERT INTO `tbl_schedule` (`id`, `code`, `description`, `instructor`, `day`, `startT`, `endT`, `course`, `year`, `section`) VALUES
+(1, 'CCS 116', 'ADVANCED WEB SYSTEMS', 'JEFFREY COCO', 'SUNDAY', '4:00 PM', '6:00 PM', 'BSCS ', 3, 'C');
 
 -- --------------------------------------------------------
 
@@ -85,12 +104,18 @@ CREATE TABLE `tbl_student` (
   `dateInserted` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `tbl_student`
+-- Table structure for table `tbl_subject`
 --
 
-INSERT INTO `tbl_student` (`id`, `firstname`, `lastname`, `midname`, `course`, `year`, `section`, `studno`, `password`, `status`, `stud_Stat`, `dateInserted`) VALUES
-(1, 'SARAH GRACE ARLYN', 'OBEN', '', 'BSCS', 3, 'C', 20191940, 'L31cia26aR8=', 'Active', 'Regular', '2021-12-08');
+CREATE TABLE `tbl_subject` (
+  `id` int(11) NOT NULL,
+  `subcode` varchar(255) NOT NULL,
+  `subunits` int(255) NOT NULL,
+  `subdesc` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -109,9 +134,21 @@ ALTER TABLE `tbl_professor`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_schedule`
+--
+ALTER TABLE `tbl_schedule`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_student`
 --
 ALTER TABLE `tbl_student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_subject`
+--
+ALTER TABLE `tbl_subject`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -128,13 +165,25 @@ ALTER TABLE `tbl_adminlogin`
 -- AUTO_INCREMENT for table `tbl_professor`
 --
 ALTER TABLE `tbl_professor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_schedule`
+--
+ALTER TABLE `tbl_schedule`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_student`
 --
 ALTER TABLE `tbl_student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_subject`
+--
+ALTER TABLE `tbl_subject`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
